223ec77f276015d1530a17953ff3dc4b70078fd0
Sneha0716 <snehamishra0013@gmail.com>
2025-12-05 13:11:14 +1300

    completed the project

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Contact from './pages/Contact'
 import About from './pages/About'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import TargetForm from './pages/TargetForm'
+import WeeklyPlan from './pages/WeeklyPlan'
 
 function App() {
 
@@ -19,6 +24,11 @@ function App() {
       <Route path='/' element={<Home/>} />
       <Route path='/contact' element={<Contact/>} />
       <Route path='/about' element={<About/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<Signup/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/target-setup' element={<TargetForm />} />
+      <Route path='/weekly-plan' element={<WeeklyPlan />} />
     </Routes>
     </BrowserRouter>
     </>
