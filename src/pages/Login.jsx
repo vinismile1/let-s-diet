1714b3b581f69651e9cf0764630161d266850d64
Sneha0716 <snehamishra0013@gmail.com>
2025-12-06 13:53:38 +1300

    fixed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import {
   signInWithEmailAndPassword,
   signInWithPopup,
 } from "firebase/auth";
-import { auth, googleProvider } from "../firebase.js";
+import { auth, googleProvider } from "../firebase_add.js";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
