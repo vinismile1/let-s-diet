89d9f2aab18d3a174a7a4d96b9561933fc636cce
Sneha0716 <snehamishra0013@gmail.com>
2026-06-22 12:46:19 +1300

    Use DATABASE_URL for Railway

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,13 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
