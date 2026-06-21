8ee3fdb68b7b07b8229f4bf1b0221bb394352cfa
Sneha0716 <snehamishra0013@gmail.com>
2026-06-22 12:34:10 +1300

    Added admin login logs

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -48,59 +48,64 @@ return res.status(500).json({
 // LOGIN ADMIN
 export const loginAdmin = (req, res) => {
 
+  console.log("LOGIN REQUEST RECEIVED");
+
   const { email, password } = req.body;
+
+  console.log(email);
 
   const sql = "SELECT * FROM admins WHERE email = ?";
 
-  db.query(
-    sql,
-    [email],
-    (err, result) => {
+  db.query(sql, [email], (err, result) => {
 
-      if (err) {
-        return res.status(500).json({
-          message: "Server Error"
-        });
-      }
+    console.log("QUERY FINISHED");
 
-      // User not found
-      if (result.length === 0) {
-        return res.status(404).json({
-          message: "User not found"
-        });
-      }
+    if (err) {
+      console.log("DB ERROR:", err);
 
-      const user = result[0];
-
-      // Compare password with hashed password
-      const isMatch = bcrypt.compareSync(
-        password,
-        user.password
-      );
-
-      if (!isMatch) {
-        return res.status(401).json({
-          message: "Invalid password"
-        });
-      }
-
-      const token = jwt.sign(
-  {
-    id: user.id,
-    email: user.email,
-    role: "admin"
-  },
-  process.env.JWT_SECRET,
-  {
-    expiresIn: "1d"
-  }
-);
-
-res.status(200).json({
-  message: "Admin Login Successful",
-  token
-});
-
+      return res.status(500).json({
+        message: "Server Error"
+      });
     }
-  );
+
+    console.log(result);
+
+    if (result.length === 0) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    const user = result[0];
+
+    const isMatch = bcrypt.compareSync(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid password"
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        role: "admin"
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d"
+      }
+    );
+
+    res.status(200).json({
+      message: "Admin Login Successful",
+      token
+    });
+
+  });
+
 };
