ccd29ff0dba1474de7f7f79316cb324873a172a0
Sneha0716 <snehamishra0013@gmail.com>
2026-06-27 10:20:26 +1300

    fixing gemini version bug

--- Backend/controllers/aiController.js
+++ Backend/controllers/aiController.js
@@ -1,37 +1,93 @@
+// import dotenv from "dotenv";
+// import { GoogleGenerativeAI } from "@google/generative-ai";
+
+// dotenv.config();
+
+// console.log("API KEY START:", process.env.GEMINI_API_KEY?.substring(0, 10));
+
+// export const askAI = async (req, res) => {
+//   try {
+//     const { message } = req.body;
+
+//     if (!message || message.trim().length === 0) {
+//       return res.status(400).json({ error: "Message is required" });
+//     }
+
+//     const model = genAI.getGenerativeModel({
+//       model: "models/gemini-1.5-flash",
+//     });
+
+//     const prompt = `
+// You are a certified nutritionist and fitness coach.
+
+// Rules:
+// - Safe advice only
+// - No extreme dieting
+// - No medical diagnosis
+// - Keep answers short and practical
+
+// User:
+// ${message}
+// `;
+
+//     const result = await model.generateContent(prompt);
+
+//     const response = result.response.text();
+
+//     return res.json({ response });
+
+//   } catch (error) {
+//     console.log("Gemini Error:", error);
+
+//     return res.status(500).json({
+//       error: error.message,
+//     });
+//   }
+// };
+
+
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+console.log(
+  "API KEY START:",
+  process.env.GEMINI_API_KEY?.substring(0, 10)
+);
+
+const genAI = new GoogleGenerativeAI(
+  process.env.GEMINI_API_KEY
+);
 
 export const askAI = async (req, res) => {
   try {
     const { message } = req.body;
 
     if (!message || message.trim().length === 0) {
-      return res.status(400).json({ error: "Message is required" });
+      return res.status(400).json({
+        error: "Message is required",
+      });
     }
 
     const model = genAI.getGenerativeModel({
-      model: "models/gemini-1.5-flash",
+      model: "gemini-2.0-flash",
     });
 
     const prompt = `
-You are a certified nutritionist and fitness coach.
+ You are a certified nutritionist and fitness coach.
 
-Rules:
-- Safe advice only
-- No extreme dieting
-- No medical diagnosis
+ Rules:
+ - Safe advice only
+ - No extreme dieting
+ - No medical diagnosis
 - Keep answers short and practical
 
-User:
+ User:
 ${message}
 `;
 
-    const result = await model.generateContent(prompt);
-
+ const result = await model.generateContent(prompt);
     const response = result.response.text();
 
     return res.json({ response });
